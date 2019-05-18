d0a7cf0fe2738b267eb0cc66c6d42648a66a856d
hongxuan <18981914907@189.cn>
2019-05-18 23:56:52 +0800

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,4 +18,13 @@ class UsersController extends Controller
     public function index(){
     	return  1;
     }
+     public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
+    }
 }
